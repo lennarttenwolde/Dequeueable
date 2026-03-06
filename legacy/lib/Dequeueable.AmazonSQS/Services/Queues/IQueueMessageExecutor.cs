b81787a347c59fdae4780ccff570d1b314f4e52e
Lennart ten Wolde <48725857+lennarttenwolde@users.noreply.github.com>
2026-03-06 18:34:54 +1300

    Remove legacy

--- legacy/lib/Dequeueable.AmazonSQS/Services/Queues/IQueueMessageExecutor.cs
+++
@@ -1,9 +0,0 @@
-﻿using Dequeueable.AmazonSQS.Models;
-
-namespace Dequeueable.AmazonSQS.Services.Queues
-{
-    internal interface IQueueMessageExecutor
-    {
-        Task ExecuteAsync(Message message, CancellationToken cancellationToken);
-    }
-}
